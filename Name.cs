a4e9921b8261635b4093417daf77352398d4a660
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-27 18:00:00 +0300

    Functions

--- Name.cs
+++ Name.cs
@@ -14,8 +14,7 @@ namespace FunctionTestTwo
     {
         [FunctionName("Name")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,string name)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -25,9 +24,7 @@ namespace FunctionTestTwo
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = "Jay ";
 
             return new OkObjectResult(responseMessage);
         }
